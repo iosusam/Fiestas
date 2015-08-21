fa1a73642e0afefc4784173e0dbaff55f4cc7881
iosusam <iosusam_93@hotmail.com>
2015-08-21 17:23:33 +0200

    Creacion login y registro

--- app/src/main/java/proyecto/fiestas/MainActivity.java
+++ app/src/main/java/proyecto/fiestas/MainActivity.java
@@ -7,17 +7,17 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         //localizamos el boton en la vista
-        final Button botonRegistro = (Button)findViewById(R.id.Registro);
-        final Button botonLogin = (Button)findViewById(R.id.Login);
+        final Button botonRegistro = (Button)findViewById(R.id.boton_registro_main);
+        final Button botonLogin = (Button)findViewById(R.id.boton_login_main);
 
         //creamos los listener de los botones
         botonRegistro.setOnClickListener(new View.OnClickListener() {
@@ -59,15 +59,43 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void conectarBBDD(){
-        setContentView(R.layout.prueba);
-    }
     //funcion que contola el login
     public void login(){
+        //Declaramos variables de la funcion
+        final EditText usuarioEditText = (EditText)findViewById(R.id.usuario_login);
+        final EditText passwordEditText = (EditText)findViewById(R.id.password_login);
+        //llamamos a la vista para mostrar el login
         setContentView(R.layout.login);
+        //Localizamos boton para enviar al servidor usuario y contraseña
+        final Button botonLogin = (Button)findViewById(R.id.boton_login);
+        //creamos los listener de los botones
+        botonLogin.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                String usuario = usuarioEditText.getText().toString();
+                String password = passwordEditText.getText().toString();
+            }
+        });
     }
+
     //funcion que controla el registro
     public void registro(){
+        //Declaramos variable de la funcion
+        final EditText usuarioEditText = (EditText)findViewById(R.id.usuario_registro);
+        final EditText passwordEditText = (EditText)findViewById(R.id.password_registro);
+        final EditText nombreEditText = (EditText)findViewById(R.id.nombre_registro);
+        final EditText apellidosEditText = (EditText)findViewById(R.id.apellidos_registro);
+        //llamamos a la vista para mostrar el login
         setContentView(R.layout.registro);
+        //Localizamos boton para enviar los datos al servidor y realizar el registro
+        final Button botonRegistro = (Button)findViewById(R.id.boton_registro);
+        //creamos los listener de los botones
+        botonRegistro.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                String usuario = usuarioEditText.getText().toString();
+                String password = passwordEditText.getText().toString();
+                String nombre = nombreEditText.getText().toString();
+                String apellidos = apellidosEditText.getText().toString();
+            }
+        });
     }
 }
